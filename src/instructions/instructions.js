c569370bc3fed2553982ad52bf8ca5f8a06d7f87
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-07-11 21:49:52 -0700

    Adds a how to play animation

--- src/instructions/instructions.js
+++ src/instructions/instructions.js
@@ -4,80 +4,17 @@ const backgroundEle = document.getElementById("background-window");
 const backgroundButton = document.getElementById("background-button");
 const xButton = document.getElementById("x-button");
 
-const nextButton = document.getElementById("next-button");
-const htp1Ele = document.getElementById("htp-1-window");
-const htp2Ele = document.getElementById("htp-2-window");
-const htp3Ele = document.getElementById("htp-3-window");
-const htp4Ele = document.getElementById("htp-4-window");
-const htp5Ele = document.getElementById("htp-5-window");
-const htp6Ele = document.getElementById("htp-6-window");
-
-const showDOMEle = ele => {
-  ele.className = ele.className.replace("-hide", "-show");
-};
-
-const hideDOMEle = ele => {
-  ele.className = ele.className.replace("-show", "-hide");
-};
-
 const showInstructionsWindow = () => {
-  showDOMEle(instructionsEle);
-  showDOMEle(xButton);
+  instructionsEle.className = instructionsEle.className.replace("-hide", "-show");
 };
 
 const hideInstructionsWindow = () => {
-  nextButton.removeEventListener("click", hideInstructionsWindow);
-  hideDOMEle(instructionsEle);
-  [...instructionsEle.children].forEach(ele => hideDOMEle(ele));
+  instructionsEle.className = instructionsEle.className.replace("-show", "-hide");
 };
-
-xButton.addEventListener("click", hideInstructionsWindow);
-
 
 const showBackground = event => {
   showInstructionsWindow();
-  showDOMEle(backgroundEle);
 };
 
 backgroundButton.addEventListener("click", showBackground);
-
-
-const showHTP1 = event => {
-  showInstructionsWindow()
-  showDOMEle(htp1Ele);
-  showDOMEle(nextButton);
-  nextButton.addEventListener("click", showHTP2);
-  nextButton.innerText = "Next!";
-};
-
-const showHTP2 = event => {
-  nextButton.removeEventListener("click", showHTP2);
-  hideDOMEle(htp1Ele);
-  showDOMEle(htp2Ele);
-  nextButton.addEventListener("click", showHTP3);
-};
-
-const showHTP3 = event => {
-  nextButton.removeEventListener("click", showHTP3);
-  hideDOMEle(htp2Ele);
-  showDOMEle(htp3Ele);
-  nextButton.addEventListener("click", showHTP4);
-};
- 
-const showHTP4 = event => {
-  nextButton.removeEventListener("click", showHTP4);
-  hideDOMEle(htp3Ele);
-  showDOMEle(htp4Ele);
-  nextButton.addEventListener("click", showHTP5);
-};
- 
-const showHTP5 = event => {
-  nextButton.removeEventListener("click", showHTP6);
-  hideDOMEle(htp5Ele);
-  showDOMEle(htp6Ele);
-  nextButton.innerText = "Let's do this!";
-  nextButton.addEventListener("click", hideInstructionsWindow);
-};
- 
-
-instructionsButton.addEventListener("click", showHTP1);
+xButton.addEventListener("click", hideInstructionsWindow);
--- src/instructions/instructions_animation.js
+++ src/instructions/instructions_animation.js
@@ -6,7 +6,8 @@ class HowToPlayAnimation {
     this.canvasEl = canvasEl;
     this.ctx = ctx;
     this.page = 0;
-    
+    this.jokesIdx = Math.floor(Math.random() * 3);
+
     this.startHTP = this.startHTP.bind(this);
     htpButton.addEventListener("click", this.startHTP);
   }
@@ -14,8 +15,8 @@ class HowToPlayAnimation {
   startHTP() {
     htpButton.removeEventListener("click", this.startHTP);
     this.page++;
-    if (this.page <= 2) {
-      setTimeout(this.startHTP, 2000);
+    if (this.page <= 19) {
+      setTimeout(this.startHTP, 4000);
     } else {
       htpButton.addEventListener("click", this.startHTP);
     }
@@ -31,6 +32,57 @@ class HowToPlayAnimation {
       case 2:
         this.objectiveIntro();
         break;
+      case 3:
+        this.healthBarIntro();
+        break;
+      case 4:
+        this.healthBarJoke();
+        break;
+      case 5:
+        this.enemiesIntro();
+        break;
+      case 6:
+        this.towersIntro();
+        break;
+      case 7:
+        this.towersUtility();
+        break;
+      case 8:
+        this.towersHint();
+        break;
+      case 9:
+        this.experimentHint();
+        break;
+      case 10:
+        this.towerSelection();
+        break;
+      case 11:
+        this.towerPlacement();
+        break;
+      case 12:
+        this.towerCancel();
+        break;
+      case 13:
+        this.towerPurchasing();
+        break;
+      case 14:
+        this.earningFlint();
+        break;
+      case 15:
+        this.purchaseMechanics1();
+        break;
+      case 16:
+        this.purchaseMechanics2();
+        break;
+      case 17:
+        this.purchaseHint();
+        break;
+      case 18:
+        this.conclusion1();
+        break;
+      case 19:
+        this.conclusion2();
+        break;
       default: 
         this.page = 0;
         break;
@@ -38,21 +90,129 @@ class HowToPlayAnimation {
   }
 
   monolithIntro() {
+    this.jokesIdx = Math.floor(Math.random() * 3);
     this.ctx.font = "64px Arial";
     this.ctx.fillStyle = "black";
     this.ctx.textAlign = "center";
-    this.ctx.fillText("This is the monolith.", 750, 500);
-    this.pulsingArrow(1100, 215, 0);
+    this.ctx.fillText("This is the monolith", 750, 500);
+    this.arrow(1150, 215, 0);
   }
 
   objectiveIntro() {
-    this.ctx.font = "64px Arial";
-    this.ctx.fillStyle = "black";
-    this.ctx.textAlign = "center";
-    this.ctx.fillText("It is your job is to protect it!", 750, 500);
+    this.monolithIntro();
+    this.ctx.fillText("It is up to you to protect it!", 750, 600);
   }
 
-  pulsingArrow(x = 0, y = 0, theta = 0) {
+  healthBarIntro() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("This is the monolith's health bar", 750, 500);
+    this.ctx.fillText("Don't let it deplete!", 750, 600)
+    this.arrow(250, 950, Math.PI * 0.5);
+    this.arrow(1250, 950, Math.PI * 0.5);
+    this.arrow(750, 950, Math.PI * 0.5);
+  }
+
+  healthBarJoke() {
+    this.healthBarIntro();
+    const jokes = [
+      "It's kind of important...",
+      "No big deal",
+      "Or else...",
+    ];
+    this.ctx.fillText(jokes[this.jokesIdx], 750, 700)
+  }
+
+  enemiesIntro() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("Enemies will attack from these four paths", 750, 500);
+    this.arrow(250, 100, Math.PI * 0.1);
+    this.arrow(175, 750, -Math.PI * 0.15);
+    this.arrow(650, 825, -Math.PI * 0.35);
+    this.arrow(1150, 825, -Math.PI * 0.65);
+  }
+
+  towersIntro() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("These are your towers!", 750, 500);
+    this.arrow(1475, 100, 0);
+    this.arrow(1475, 238, 0);
+    this.arrow(1475, 375, 0);
+  }
+
+  towersUtility() {
+    this.towersIntro();
+    this.ctx.fillText("Towers will target and damage enemies in range", 750, 600);
+  }
+  
+  towersHint() { 
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("Each tower has its own srengths and weaknesses", 750, 500);
+  }
+
+  experimentHint() { 
+    this.towersHint();
+    this.ctx.fillText("Experiment!", 750, 600);
+  }
+
+  towerSelection() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("Click to select a tower", 750, 400);
+  }
+
+  towerPlacement() {
+    this.towerSelection();
+    this.ctx.fillText("Then, click on the map to place the tower", 750, 500);
+  }
+
+  towerCancel() {
+    this.towerPlacement();
+    this.ctx.fillText("Or, click outside the map to cancel", 750, 600);
+  }
+
+  towerPurchasing() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("Towers are not free! You must have enough flint", 750, 500);
+  }
+
+  earningFlint() {
+    this.towerPurchasing();
+    this.ctx.fillText("Stopping enemies rewards you with more flint", 750, 600);
+  }
+
+  purchaseMechanics1() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("The more towers you buy,", 750, 400);
+  }
+
+  purchaseMechanics2() {
+    this.purchaseMechanics1()
+    this.ctx.fillText("the more expensive they get", 750, 500);
+  }
+
+  purchaseHint() {
+    this.purchaseMechanics2();
+    this.ctx.fillText("Spend wisely and diversify your arsenal!", 750, 600);
+  }
+
+  conclusion1() {
+    this.ctx.fillStyle = "black";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText("Place towers strategically around the map!", 750, 500);
+  }
+
+  conclusion2() {
+    this.conclusion1();
+    this.ctx.fillText("If the monolith runs out of health, you lose!", 750, 600);
+  }
+
+  arrow(x = 0, y = 0, theta = 0) {
     this.ctx.save();
 
     this.ctx.translate(x, y);
@@ -62,15 +222,16 @@ class HowToPlayAnimation {
     this.ctx.lineWidth = 10;
     this.ctx.strokeStyle = "black";
     this.ctx.moveTo(0, 0);
-    this.ctx.lineTo(0, -10);
-    this.ctx.lineTo(100, -10);
-    this.ctx.lineTo(100, -30);
-    this.ctx.lineTo(150, 0);
-    this.ctx.lineTo(100, 30);
-    this.ctx.lineTo(100, 10);
-    this.ctx.lineTo(0, 10);
-    this.ctx.lineTo(0, 0);
+    this.ctx.lineTo(-50, 30);
+    this.ctx.lineTo(-50, 10);
+    this.ctx.lineTo(-150, 10);
+    this.ctx.lineTo(-150, -10);
+    this.ctx.lineTo(-50, -10);
+    this.ctx.lineTo(-50, -30);
+    this.ctx.closePath();
     this.ctx.stroke();
+    this.ctx.fillStyle = "yellow";
+    this.ctx.fill();
 
     this.ctx.restore();
   };
